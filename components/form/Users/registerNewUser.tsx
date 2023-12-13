9b57dcfb0cfc6625e05c1acfdf0c92a163e65c78
Brisna A. Paez M <124035708+Brisnayu@users.noreply.github.com>
2023-12-13 21:35:44 +0100

    user page, register cloudinary

--- components/form/Users/registerNewUser.tsx
+++ components/form/Users/registerNewUser.tsx
@@ -16,6 +16,8 @@ const RegisterNewUser = ({ setUser }: RegisterNewUserProps) => {
     password: "",
   });
 
+  const [userValid, setUserValid] = useState(true);
+
   const {
     register,
     handleSubmit,
@@ -45,19 +47,41 @@ const RegisterNewUser = ({ setUser }: RegisterNewUserProps) => {
   };
 
   const onSubmit: SubmitHandler<RegisterNewUserForm> = async (formData) => {
-    // console.log("ESTOS SON LOS VALORES DEL FORMULARIO", formData);
+    console.log("ESTOS SON LOS VALORES DEL FORMULARIO", formData);
 
     const dataParse = JSON.stringify(formData);
 
+    console.log("antes del cambio", formData);
+    console.log("después del cambio: dataParse", dataParse);
+
+    const data = new FormData();
+    data.append("name", formData.name);
+    data.append("email", formData.email);
+    data.append("password", formData.password);
+    data.append("avatar", formData.avatar);
+
+    console.log(formData.email);
+
+    const response = await fetch("http://localhost:4001/api/user").then((res) =>
+      res.json()
+    );
+
+    console.log("función!", response.data);
+
+    for (const emailUser of response.data) {
+      console.log(emailUser.email);
+      if (emailUser.email === formData.email) {
+        console.log("ESTO ES IGUAL!");
+        return setUserValid(false);
+      }
+    }
+
     try {
       const response = await fetch(
-        "http://localhost:4001/api/user/auth/login",
+        "http://localhost:4001/api/user/auth/register",
         {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
+          body: data,
         }
       );
 
@@ -69,6 +93,7 @@ const RegisterNewUser = ({ setUser }: RegisterNewUserProps) => {
         const authToken = userJson.token;
         // console.log("AQUÍ ESTÁ LA RESPUETA", authToken)
         localStorage.setItem("authToken", authToken);
+        console.log("TODO OK", userJson);
         // window.location.href = "/form/register";
       } else {
         console.log("ERROR", response.status);
@@ -89,28 +114,38 @@ const RegisterNewUser = ({ setUser }: RegisterNewUserProps) => {
             <Controller
               name="name"
               control={control}
-              render={({ field }) => <input type="password" {...field} />}
+              render={({ field }) => (
+                <>
+                  <input type="name" {...register("name")} />
+                  {errors.name && (
+                    <p style={{ color: "red" }}>{errors.name.message}</p>
+                  )}
+                </>
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: "Es obligatorio escribir un nombre de usuario",
+                },
+              }}
             />
           </div>
 
           <div className={styles.containerInput}>
             <label>Avatar</label>
             <Controller
-            name="avatar"
-            control={control}
-            render={({ field }) => (
-              
+              name="avatar"
+              control={control}
+              render={({ field }) => (
                 <input
                   type="file"
                   id="image"
                   onChange={(e) => {
-                    field.onChange(e.target.files?.[0]),
-                    handleFileChange(e);
+                    field.onChange(e.target.files?.[0]), handleFileChange(e);
                   }}
                 />
-              
-            )}
-          />
+              )}
+            />
           </div>
 
           <div className={styles.containerInput}>
@@ -118,7 +153,27 @@ const RegisterNewUser = ({ setUser }: RegisterNewUserProps) => {
             <Controller
               name="email"
               control={control}
-              render={({ field }) => <input type="text" {...field} />}
+              render={({ field }) => (
+                <>
+                  <input type="text" {...field} />
+                  {errors.email && (
+                    <p style={{ color: "red" }}>{errors.email.message}</p>
+                  )}
+                  {userValid === false && (
+                    <p style={{ color: "red" }}>Usuario ya registrado</p>
+                  )}
+                </>
+              )}
+              rules={{
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Introduce un email válido.",
+                },
+                required: {
+                  value: true,
+                  message: "Es obligatorio escribir un email",
+                },
+              }}
             />
           </div>
 
@@ -127,7 +182,29 @@ const RegisterNewUser = ({ setUser }: RegisterNewUserProps) => {
             <Controller
               name="password"
               control={control}
-              render={({ field }) => <input type="password" {...field} />}
+              render={({ field }) => (
+                <>
+                  <input type="password" {...field} />
+                  {errors.password && (
+                    <p style={{ color: "red" }}>{errors.password.message}</p>
+                  )}
+                </>
+              )}
+              rules={{
+                minLength: {
+                  value: 6,
+                  message: "La contraseña debe tener al menos 6 caracteres.",
+                },
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z])/,
+                  message:
+                    "La contraseña debe tener al menos una letra mayúsucula y una minúscula",
+                },
+                required: {
+                  value: true,
+                  message: "Escriba una contraseña",
+                },
+              }}
             />
           </div>
 
